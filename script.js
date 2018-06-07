525662ddc0cec9f8cd45c37f78a48b526208d27b
richardse08 <eric.richards.finance@gmail.com>
2018-06-07 17:16:35 -0400

    Changing counter function

--- script.js
+++ script.js
@@ -50,7 +50,7 @@ $(document).ready(function(){
     // When any divs with icons class is clicked
     // Grab its id and pass it into windowLoader to render html window
     $(".footer-icon").click(function(){
-        if (windowCounter <= 3) {
+        if (windowCounter < 3) {
             windowLoader(this.id);  
             $('.about-me').addClass('display-none');
             windowCounter++;
